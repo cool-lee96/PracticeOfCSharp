b6a609175a140fe31fb1accbf86f587857a4d8a3
cool-lee96 <142783291+cool-lee96@users.noreply.github.com>
2025-03-28 09:34:49 +0900

    [Bronze V] Title: Micromasters, Time: 52 ms, Memory: 5460 KB -BaekjoonHub

---
+++ C#/백준/Bronze/33178. Micromasters/Micromasters.cs
@@ -0,0 +1,13 @@
+/* 33170 */
+
+class ブラックジャック
+{
+    static void Main()
+    {
+        // 문제 입력 받기
+        int input_student = int.Parse(Console.ReadLine()?.Trim());
+
+        // 결과 출력
+        Console.Write(input_student / 10);
+    }
+}
