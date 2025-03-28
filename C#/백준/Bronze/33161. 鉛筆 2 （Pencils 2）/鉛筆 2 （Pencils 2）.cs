04ee2adefcdf3d79056d05a86ce2b9a5f1d90156
cool-lee96 <142783291+cool-lee96@users.noreply.github.com>
2025-03-28 09:27:21 +0900

    [Bronze V] Title: 鉛筆 2 (Pencils 2), Time: 48 ms, Memory: 5468 KB -BaekjoonHub

---
+++ C#/백준/Bronze/33161. 鉛筆 2 （Pencils 2）/鉛筆 2 （Pencils 2）.cs
@@ -0,0 +1,13 @@
+/* 33161 */
+
+class 鉛筆_2
+{
+    static void Main()
+    {
+        // 문제 입력 받기
+        var input_yen = int.Parse(Console.ReadLine()?.Trim());
+
+        // 결과 출력
+        Console.Write(input_yen / 5);
+    }
+}
